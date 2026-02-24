6f71b54dead9d2797ac77afb2e56273c73c90612
cesiumloser <st.nikitka.2002@gmail.com>
2026-02-24 13:05:19 +0300

    Делаю Карты

---
+++ CardData/CardData.gd
@@ -0,0 +1,13 @@
+extends Resource
+class_name CardData
+
+@export_group("Visuals")
+@export var title: String = "Название"
+@export_multiline var description: String = "Описание карты..."
+@export var icon: Texture2D
+
+@export_group("Stats")
+@export var cost: int = 1 # Сколько маны стоит
+@export var damage: int = 0 # Урон по телке (XOXO Bar)
+@export var heal: int = 0 # Хилл
+@export var shield: int = 0 # Доп. ХП
