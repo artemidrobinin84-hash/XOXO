cb4abd7f4b10a90dd9ff65067ff81dd8434f54d1
Name <artemidrobinin84@gmail.com>
2026-02-25 18:56:14 +0300

    Sound

--- UI/PauseMenu/pause_menu.gd
+++ UI/PauseMenu/pause_menu.gd
@@ -7,11 +7,13 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("Esc"):
+		print("Работает")
 		toggle_pause()
 
 func toggle_pause():
 	get_tree().paused = !get_tree().paused
 	visible = get_tree().paused
+	show()
 
 func _on_resume_button_pressed():
 	toggle_pause()
