9c1669820008e3e7fd7d5ed2bb6fdad05f5b24ce
Name <artemidrobinin84@gmail.com>
2026-02-25 17:50:15 +0300

    Коммент

--- UI/PauseMenu/pause_menu.gd
+++ UI/PauseMenu/pause_menu.gd
@@ -2,20 +2,15 @@ extends Control
 
 
 func _ready():
-	# Скрываем меню при старте
 	hide()
-	# Убеждаемся, что игра НЕ на паузе
 	get_tree().paused = false
 
 func _input(event):
-	# Проверяем нажатие Esc
 	if event.is_action_pressed("Esc"):
 		toggle_pause()
 
 func toggle_pause():
-	# Переключаем паузу в движке
 	get_tree().paused = !get_tree().paused
-	# Показываем или скрываем меню
 	visible = get_tree().paused
 
 func _on_resume_button_pressed():
