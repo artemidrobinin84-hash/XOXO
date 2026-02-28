36a3792ed8c348bc504ab9cfe60a68b6a0200318
cesiumloser <st.nikitka.2002@gmail.com>
2026-02-28 19:30:22 +0300

    Делаю Тутор

--- lvl/victorytutor.gd
+++ lvl/victorytutor.gd
@@ -1,15 +1,12 @@
 extends CanvasLayer
 
-@onready var next_level_btn = $HBoxContainer/NextLevelButton
 @onready var main_menu_btn = $HBoxContainer/MainMenuButton
 @onready var click_sound = $"../Click"
 @onready var boss = get_tree().get_first_node_in_group("enemy")
 
 func _ready():
 	hide()
-	$HBoxContainer/NextLevelButton.mouse_entered.connect(_on_button_hover)
 	$HBoxContainer/MainMenuButton.mouse_entered.connect(_on_button_hover)
-	next_level_btn.pressed.connect(_on_next_level_pressed)
 	main_menu_btn.pressed.connect(_on_main_menu_pressed)
 	if boss:
 		boss.boss_defeated.connect(_on_victory)
@@ -22,7 +19,7 @@ func _on_next_level_pressed():
 	click_sound.play()
 	await get_tree().create_timer(0.15).timeout
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://lvl/lvl3.tscn")
+	get_tree().change_scene_to_file("res://UI/MainMenu/MainMenu.tscn")
 
 func _on_main_menu_pressed():
 	click_sound.play()
