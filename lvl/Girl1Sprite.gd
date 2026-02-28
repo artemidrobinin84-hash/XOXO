dbe23111c41276b680c7868ba6ac79556b492f36
cesiumloser <st.nikitka.2002@gmail.com>
2026-02-28 13:51:10 +0300

    Небольшие корректировки

--- lvl/Girl1Sprite.gd
+++ lvl/Girl1Sprite.gd
@@ -1,30 +1,24 @@
 extends SpineSprite
 
-# Путь к прогрессбару (проверь, чтобы путь соответствовал твоей сцене)
-@onready var health_bar = get_node("/root/lvl1/XOXOProgressBar/ProgressBar")
+@onready var health_bar = get_node("../ProgressBar/XOXOProgressBar")
 @onready var phase_sound = $PhaseSound
 var current_skin = ""
 
 func _ready():
-	# 1. Запуск анимации idle. 
-	# Правильный синтаксис: set_animation(название, loop, track)
 	var animation_state = get_animation_state()
 	if animation_state:
 		animation_state.set_animation("idle", true, 0)
 	
-	# 2. Первичная настройка скина при старте
 	if health_bar:
 		_update_visuals(health_bar.value)
 
 func _process(_delta):
-	# Следим за здоровьем и обновляем стадию (level1, level2, level3)
 	if health_bar:
 		_update_visuals(health_bar.value)
 
 func _update_visuals(hp_value: float):
 	var target_skin = ""
 	
-	# Твои условия по фазам здоровья
 	if hp_value >= 67:
 		target_skin = "level1"
 	elif hp_value >= 34:
@@ -32,20 +26,16 @@ func _update_visuals(hp_value: float):
 	else:
 		target_skin = "level3"
 	
-	# Если скин уже установлен, выходим из функции
 	if current_skin == target_skin:
 		return
 		
 	var skeleton = get_skeleton()
 	if skeleton:
-		# Устанавливаем новый скин из Spine
 		skeleton.set_skin_by_name(target_skin)
-		# Сбрасываем позу, чтобы новые элементы скина отобразились корректно
 		skeleton.set_to_setup_pose()
 		if current_skin != "" and phase_sound:
 			print("Смена фазы: играю звук")
 			phase_sound.play()
 		
-		# 2. И только теперь записываем новый скин в переменную
 		current_skin = target_skin
 		print("Фаза изменена на: ", target_skin)
--- lvl/victory.gd
+++ lvl/victory.gd
@@ -3,26 +3,20 @@ extends CanvasLayer
 @onready var next_level_btn = $HBoxContainer/NextLevelButton
 @onready var main_menu_btn = $HBoxContainer/MainMenuButton
 @onready var click_sound = $"../Click"
-# Путь к боссу, чтобы подписаться на его смерть
 @onready var boss = get_tree().get_first_node_in_group("enemy")
 
 func _ready():
-	# Скрываем слой при запуске
 	hide()
 	$HBoxContainer/NextLevelButton.mouse_entered.connect(_on_button_hover)
 	$HBoxContainer/MainMenuButton.mouse_entered.connect(_on_button_hover)
-	# Подключаем кнопки кодом (можно и через инспектор)
 	next_level_btn.pressed.connect(_on_next_level_pressed)
 	main_menu_btn.pressed.connect(_on_main_menu_pressed)
-	# Подписываемся на сигнал победы из твоего GirlHitbox
 	if boss:
 		boss.boss_defeated.connect(_on_victory)
 func _on_button_hover() -> void:
 	click_sound.play()
 func _on_victory():
-	show() # Показываем экран победы
-	# Если нужно остановить игру, раскомментируй строку ниже:
-	# get_tree().paused = true 
+	show()
 
 func _on_next_level_pressed():
 	click_sound.play()
